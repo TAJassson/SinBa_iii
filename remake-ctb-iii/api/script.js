0b33e7f90f954fe23c262a477d86a8695bb8bd17
Jason Tse <93077307+TAJassson@users.noreply.github.com>
2023-12-28 09:32:21 +0800

    1

--- remake-ctb-iii/api/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#submitButton').click(function() {
-        var selectedLang = $('#languageSelect').val();
-        window.location.href = 'home.php?lang=' + selectedLang;
-    });
-});
